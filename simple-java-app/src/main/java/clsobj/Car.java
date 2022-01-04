387f8e6b2933142ea0b3c566750a47f590e8581d
ananda.sawant <ananda.sawant@collabera.com>
2022-01-04 03:24:47 -0800

    test cases : for Object and Constructor

---
+++ simple-java-app/src/main/java/clsobj/Car.java
@@ -0,0 +1,14 @@
+package clsobj;
+
+public class Car {
+
+    public int speed;
+
+    public Car()
+    { speed =10;}
+
+    public Car(int sp)
+    {
+        speed =sp;
+    }
+}
--- simple-java-app/src/test/java/AdditionTest.java
+++
@@ -1,7 +0,0 @@
-public class AdditionTest {
-
-    public int addition(int a,int b)
-    {
-        return (a+b);
-    }
-}
