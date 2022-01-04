e58a1ba0b4b0fcc7c3ce164575bf352f175a02c5
ananda.sawant <ananda.sawant@collabera.com>
2022-01-04 04:38:29 -0800

    test cases : Unit Test cases for is a relations

--- simple-java-app/src/test/java/BmwTest.java
+++ simple-java-app/src/test/java/BmwTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import clsobj.Bmw;
+import clsobj.Car;
 public class BmwTest {
     @DisplayName("Testing Object NullAbility")
     @Test
@@ -19,5 +20,22 @@ public class BmwTest {
         Bmw bmw =new Bmw();
         Assertions.assertNotNull(bmw);
 
+    }
+    @DisplayName("Test is Bmw is a Car")
+    @Test
+    void testBmwObjectCar() {
+        Bmw bmw = new Bmw();
+        boolean expected = true;
+        boolean actual = bmw instanceof Car;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @DisplayName("Test Car is Not a BMW")
+    @Test
+    void testCarObjectNotBmw() {
+        Car car = new Car();
+        boolean expected = false;
+        boolean actual = car instanceof Bmw;
+        Assertions.assertEquals(expected, actual);
     }
 }
