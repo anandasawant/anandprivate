ceaf3a624027ec1b07e6ef20b45f8eac46cc16a6
ananda.sawant <ananda.sawant@collabera.com>
2022-01-07 15:11:38 +0530

    Abstract Factory

--- Day5/design-patterns/src/main/java/factory/CardFactory.java
+++ Day5/design-patterns/src/main/java/factory/CardFactory.java
@@ -1,11 +1,15 @@
 package factory;
 
-public abstract class CardFactory {
+public class CardFactory extends Factory{
 
     public static CardProvider getCard(String bank) {
         if(bank.equals("pqr")) return new PqrCard();
         if(bank.equals("abc")) return new AbcCard();
         if(bank.equals("xyz")) return new XyzCard();
         throw new InvalidBankException("ATM Card Facility is not provided by "+bank);
+    }
+
+    public static Factory newInstance() {
+        return new CardFactory();
     }
 }
