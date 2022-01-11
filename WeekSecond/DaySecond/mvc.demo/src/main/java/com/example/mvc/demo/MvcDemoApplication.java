35d67eb168e756998a2688b2ac088b12aa837187
ananda.sawant <ananda.sawant@collabera.com>
2022-01-11 16:00:10 +0530

    Spring mvc:add operation

--- WeekSecond/DaySecond/mvc.demo/src/main/java/com/example/mvc/demo/MvcDemoApplication.java
+++ WeekSecond/DaySecond/mvc.demo/src/main/java/com/example/mvc/demo/MvcDemoApplication.java
@@ -1,14 +1,14 @@
 package com.example.mvc.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-public class Application {
+public class MvcDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(MvcDemoApplication.class, args);
 	}
 
 }
