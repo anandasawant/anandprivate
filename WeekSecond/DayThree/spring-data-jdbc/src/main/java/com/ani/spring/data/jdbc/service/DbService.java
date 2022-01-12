42e2007aa65b0972c1693e11b4c467b22ea8e2cf
ananda.sawant <ananda.sawant@collabera.com>
2022-01-12 14:17:33 +0530

    java jdbc rowmapper

--- WeekSecond/DayThree/spring-data-jdbc/src/main/java/com/ani/spring/data/jdbc/service/DbService.java
+++ WeekSecond/DayThree/spring-data-jdbc/src/main/java/com/ani/spring/data/jdbc/service/DbService.java
@@ -1,15 +1,15 @@
 package com.ani.spring.data.jdbc.service;
+import com.ani.spring.data.jdbc.domain.Emp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Service;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class DbService {
@@ -28,6 +28,37 @@ public class DbService {
             ps.setBoolean(4, false);
             return ps;
         });
+    }
+    public void saveOpV2(Emp emp) {
+        var sql = "insert into emp_info values (?, ?, ? ,? )";
+        template.update(sql, emp.getId(), emp.getName(), emp.getDob(), emp.getManager());
+    }
+
+    public List<Emp> findEmployees() {
+        var sql = "select * from emp_info";
+
+        List<Emp> employees = template.query(sql, new RowMapper<Emp>() {
+            @Override
+            public Emp mapRow(ResultSet rs, int rowNum) throws SQLException {
+                var emp = new Emp();
+                emp.setId(rs.getInt("emp_id"));
+                emp.setName(rs.getString("emp_name"));
+                emp.setDob(rs.getDate("dob").toString());
+                emp.setManager(rs.getBoolean("is_manager"));
+
+                return emp;
+            }
+        });
+
+        employees = template.query(sql, (rs, rowNum) -> new Emp(
+                        rs.getInt("emp_id"),
+                        rs.getString("emp_name"),
+                        rs.getDate("dob").toString(),
+                        rs.getBoolean("is_manager")
+                )
+        );
+
+        return employees;
     }
 
 }
