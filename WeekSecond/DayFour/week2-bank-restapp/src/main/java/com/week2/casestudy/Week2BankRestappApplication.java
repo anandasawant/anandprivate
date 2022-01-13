202a8333e130f9a655073fdab8489b4fd522850c
ananda.sawant <ananda.sawant@collabera.com>
2022-01-13 12:16:46 +0530

    Case Study Arcetecture

---
+++ WeekSecond/DayFour/week2-bank-restapp/src/main/java/com/week2/casestudy/Week2BankRestappApplication.java
@@ -0,0 +1,13 @@
+package com.week2.casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2BankRestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week2BankRestappApplication.class, args);
+	}
+
+}
---
+++ WeekSecond/DayFour/week2-bank-restapp/src/test/java/com/week2/casestudy/Week2BankRestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week2.casestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2BankRestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
