23d9ba753d41ce00c6a98466b860be609032744a
ananda.sawant <ananda.sawant@collabera.com>
2022-01-14 11:56:05 +0530

    UnitTest : service layer

---
+++ WeekSecond/DayFive/unit-testing-demo/src/main/java/com/ani/testing/service/CarServiceImpl.java
@@ -0,0 +1,10 @@
+package com.ani.testing.service;
+
+import com.ani.testing.domain.Car;
+
+public class CarServiceImpl implements CarService{
+    @Override
+    public Car saveCar(Car car) {
+        return car;
+    }
+}
