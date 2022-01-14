af08d81d480cde24560ad3cc220b2c1963a3a040
ananda.sawant <ananda.sawant@collabera.com>
2022-01-14 12:30:27 +0530

    UnitTest : service layer tests

--- WeekSecond/DayFive/unit-testing-demo/src/main/java/com/ani/testing/service/CarServiceImpl.java
+++ WeekSecond/DayFive/unit-testing-demo/src/main/java/com/ani/testing/service/CarServiceImpl.java
@@ -1,10 +1,19 @@
 package com.ani.testing.service;
 
 import com.ani.testing.domain.Car;
+import exception.InvalidIdException;
+
+import java.util.Optional;
 
 public class CarServiceImpl implements CarService{
     @Override
     public Car saveCar(Car car) {
         return car;
+    }
+
+    @Override
+    public Optional<Car> findCarById(Long id) {
+        if(id <= 0) throw new InvalidIdException("Id must be positive : "+id);
+        return Optional.of(new Car());
     }
 }
